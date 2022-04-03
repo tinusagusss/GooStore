42996b7cbfd6757b4aa5f1eda5c2ac12cc91c156
Agus Tinus Turnip <agus.tinus@mhs.itenas.ac.id>
2022-04-03 22:39:30 +0700

    Add ConnectionManager

---
+++ src/com/itenas/pemdas/controller/ConnectionManager.java
@@ -0,0 +1,37 @@
+package com.itenas.pemdas.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author agus
+ */
+public class ConnectionManager {
+
+    private Connection con;
+    private final String Driver = "com.mysql.jdbc.Driver";
+    private final String url = "jdbc:mysql://localhost:3306/pbdstore_162020016";
+    private final String Username = "root";
+    private final String Password = "P@ssw0rd.01";
+
+    public Connection LogOn() {
+        try {
+            Class.forName(Driver).newInstance();
+            con = DriverManager.getConnection(url, Username, Password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+    public void LogOff() {
+        try {
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
